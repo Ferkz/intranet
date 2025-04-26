6246edb642331d97c315248656564da4a3208e6f
Ferkz <fermon1952@gmail.com>
2025-04-25 21:49:08 -0400

    add new color for intranet

--- intranet/src/app/components/footer/footer.component.ts
+++ intranet/src/app/components/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import {faFacebook, faInstagram, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import {faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -11,6 +12,8 @@ export class FooterComponent implements OnInit {
   faFacebook = faFacebook;
   faInstagram = faInstagram;
   faLinkedin = faLinkedin;
+  faPolicy = faBookOpen
+
 
   constructor(library: FaIconLibrary) {
     library.addIcons();
